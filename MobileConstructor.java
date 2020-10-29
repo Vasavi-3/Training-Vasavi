7681fc4a1a1eb99ef18a6b4dcb9b3099c505ea5b
Vasavi-3 <73581130+Vasavi-3@users.noreply.github.com>
2020-10-29 19:34:19 +0530

    Add files via upload

---
+++ MobileConstructor.java
@@ -0,0 +1,33 @@
+class Mobile
+{
+int display;
+String ram;
+String company name;
+int no of front cameras;
+Mobile(int display,String ram,String company name,int no of front cameras)
+{
+this.display=display;
+this.ram=ram;
+this.company name=company name;
+this.no of front cameras=no of front cameras;
+this.display()'
+}
+void display()
+{
+System.out.println("=====Mobile Details=====");
+System.out.println("ram="+ram);
+System.out.println("Mobile display in inches=" + dispaly);
+System.out.println("company name= "+ company name);
+System.out.println("no of front cameras= "+ no of front cameras);
+}
+}
+public class MobileConstructor
+{
+public static void main(String args[])
+{
+Mobile Realme=new Mobile("4gb","4g","3","realmex");
+Mobile vivo-newMobile("6gb","4g","4","vivov7");
+}
+}
+
+
