f1f2f0701dc7bd8c04f0c4af0a0cc30b6d492caf
Vasavi-3 <73581130+Vasavi-3@users.noreply.github.com>
2020-11-20 19:11:11 +0530

    Add files via upload

---
+++ SleepDemo.java
@@ -0,0 +1,28 @@
+package com.ex;
+
+public class SleepDemo extends Thread
+{
+	public void run(){
+		for(int i=1;i<=10;i++)
+		{
+			try
+			{
+				Thread.sleep(1000);
+			}
+			catch(InterrupedException a)
+			{
+				a.printStackTrace();
+				
+			}
+			System.out.println(i);
+		}
+		public static void main(String args[])
+		{
+			SleepDemo x=new SleepDemo();
+			SleepDemo y=new sLeepDemo();
+			x.start();
+			y.start();
+		}
+}
+
+}
